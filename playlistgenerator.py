d9f13c4d55070a79688d992d4c0e3ab00a44ef1c
Silas Bempong <silasbempong@gmail.com>
2018-08-22 16:07:11 +0000

    Parsing with BeautifulSoup added

--- playlistgenerator.py
+++ playlistgenerator.py
@@ -1,7 +1,20 @@
 #===== SCRIPT GENESIS===========
+
+from bs4 import BeautifulSoup 
+
 desired_artists = ['Imagine Dragons', 'One Republic', 'Bastille', 'Marshmello','Owl City',
                    'Kygo', 'Drake', 'J Cole', 'Hillsong Young & Free',
                    'ColdPlay', 'Kwesi Arthur', 'Sarkodie', 'Manifest',
                    'Khalid', 'Stormzy', 'Jon Bellion', 'The Chainsmokers']
 
 desired_artists.sort()
+
+try:
+    songRequest = requests.get('')
+    topSongsHtml = songRequest.text
+except:
+    print('error getting top 50 song list html')
+    
+# parsing with BeautifulSoup
+html_tracks =  BeautifulSoup(topSongsHtml, 'html.parser')
+        

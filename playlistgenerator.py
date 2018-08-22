7d042c2721b82f93c77d994d38dea7a35b450cfd
Silas Bempong <silasbempong@gmail.com>
2018-08-22 15:14:03 +0000

    List of Playlist artists

---
+++ playlistgenerator.py
@@ -0,0 +1,7 @@
+#===== SCRIPT GENESIS===========
+desired_artists = ['Imagine Dragons', 'One Republic', 'Bastille', 'Marshmello','Owl City',
+                   'Kygo', 'Drake', 'J Cole', 'Hillsong Young & Free',
+                   'ColdPlay', 'Kwesi Arthur', 'Sarkodie', 'Manifest',
+                   'Khalid', 'Stormzy', 'Jon Bellion', 'The Chainsmokers']
+
+desired_artists.sort()
